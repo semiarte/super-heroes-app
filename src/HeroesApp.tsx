176fa3a81304737d444d7b84f3f88780964fcae3
José Miguel Pérez Buenaño <elsemi6@gmail.com>
2026-09-18 13:37:50 +0200

    favorites context

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -2,14 +2,17 @@ import { RouterProvider } from 'react-router'
 import { appRouter } from './router/app.router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { FavoriteHeroProvider } from './heroes/context/FavoriteHeroContext'
 
 const queryClient = new QueryClient()
 
 export const HeroesApp = () => {
     return (
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={appRouter} />
-            <ReactQueryDevtools initialIsOpen={false} />
+            <FavoriteHeroProvider>
+                <RouterProvider router={appRouter} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </FavoriteHeroProvider>
         </QueryClientProvider>
     )
 }
--- src/heroes/components/HeroGridCard.tsx
+++ src/heroes/components/HeroGridCard.tsx
@@ -2,10 +2,12 @@ import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardHeader, CardContent } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
-
-import { Heart, Eye, Zap, Brain, Gauge, Shield } from "lucide-react"
-import type { Hero } from "../types/hero.interface"
 import { useNavigate } from "react-router"
+import { use } from "react"
+import { Heart, Eye, Zap, Brain, Gauge, Shield } from "lucide-react"
+
+import type { Hero } from "../types/hero.interface"
+import { FavoriteHeroContext } from "../context/FavoriteHeroContext"
 
 interface Props {
     hero: Hero;
@@ -14,6 +16,7 @@ interface Props {
 export const HeroGridCard = ({ hero }: Props) => {
 
     const navigate = useNavigate();
+    const { isFavorite, toggleFavorite } = use(FavoriteHeroContext);
 
     const handleClick = () => {
         navigate(`/heroes/${hero.slug}`);
@@ -41,8 +44,14 @@ export const HeroGridCard = ({ hero }: Props) => {
                 <Badge className="absolute top-3 right-3 text-xs bg-blue-600 text-white">{hero.universe}</Badge>
 
                 {/* Favorite button */}
-                <Button size="sm" variant="ghost" className="absolute bottom-3 right-3 bg-white/90 hover:bg-white">
-                    <Heart className="h-4 w-4 fill-red-500 text-red-500" />
+                <Button
+                    size="sm"
+                    variant="ghost"
+                    className="absolute bottom-3 right-3 bg-white/90 hover:bg-white"
+                    onClick={() => toggleFavorite(hero)}
+                >
+                    <Heart className={`h-4 w-4  ${isFavorite(hero) ? 'fill-red-500 text-red-500' : 'text-gray-500'
+                        }`} />
                 </Button>
 
                 {/* View details button */}
